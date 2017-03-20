40b5c96ea6bc9bc6e5229534808f966d0da86936
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-03-20 01:41:09 -0700

    Present newer apache first

--- omnibus-apache_upgrade_test/config/software/apache.rb
+++ omnibus-apache_upgrade_test/config/software/apache.rb
@@ -8,14 +8,6 @@ license_file "Apache24/LICENSE.txt"
 zip_basedir='Apache24'
 
 case version
-when "2.4.23"
-  if windows_arch_i386?
-    url = 'https://www.apachelounge.com/download/VC10/binaries/httpd-2.4.23-win32.zip'
-    md5 = 'ff26d47167e271f4de9d3bd194056225'
-  else
-    url = 'https://www.apachelounge.com/download/VC10/binaries/httpd-2.4.23-win64.zip'
-    md5 =  '84d7465763468d8d47d5c725b3e79171'
-  end
 when "2.4.25"
   if windows_arch_i386?
     url = 'https://www.apachelounge.com/download/VC14/binaries/httpd-2.4.25-win32-VC14.zip'
@@ -23,6 +15,14 @@ when "2.4.25"
   else
     url = 'https://www.apachelounge.com/download/VC14/binaries/httpd-2.4.25-win64-VC14.zip'
     md5 = 'eb6642b253fcf04d288c0f518f131acd'
+  end
+when "2.4.23"
+  if windows_arch_i386?
+    url = 'https://www.apachelounge.com/download/VC10/binaries/httpd-2.4.23-win32.zip'
+    md5 = 'ff26d47167e271f4de9d3bd194056225'
+  else
+    url = 'https://www.apachelounge.com/download/VC10/binaries/httpd-2.4.23-win64.zip'
+    md5 =  '84d7465763468d8d47d5c725b3e79171'
   end
 end
 
