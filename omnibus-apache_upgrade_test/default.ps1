0a9650283d2924087ef05bfb61dc4ffd59d1097d
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-03-20 11:41:06 -0700

    Whitespace

--- omnibus-apache_upgrade_test/default.ps1
+++ omnibus-apache_upgrade_test/default.ps1
@@ -8,35 +8,35 @@ Task TestApacheUpgrade {
     Assert(!(test-path $httpd_path)) "Expecting $httpd_path should be gone from uninstall"
 
     # apache2.4.23
-    (Get-Content config/software/apache.rb) `
-	  -replace 'default_version "2\.\d+\.\d+"', 'default_version "2.4.23"' `
+    (Get-Content config/software/apache.rb)
+    -replace 'default_version "2\.\d+\.\d+"', 'default_version "2.4.23"' `
 	  | Set-Content -Encoding ascii config/software/apache.rb
 
     ruby bin/omnibus build apache_upgrade_test --log-level warn
-    $msi=(gci $pwd/pkg/apache-upgrade-test*.msi | select fullname -last 1).FullName
+    $msi = (gci $pwd/pkg/apache-upgrade-test*.msi | select fullname -last 1).FullName
     rm -ea 0 -force install_apache23.log
     Start-Process -Wait -FilePath msiexec -ArgumentList /i, `
 	  "$msi", /qn, /l*v, install_apache23.log, PROJECTLOCATION='C:\windows\temp\apache-upgrade-test'
 
     $output = & "C:\windows\temp\apache-upgrade-test\apache\bin\httpd.exe" -version
     $regex_version = '((\d+\.)?(\d+\.)?(\*|\d+))'
-    $version = [regex]::match($output,"Apache/$regex_version").Groups[1].Value
+    $version = [regex]::match($output, "Apache/$regex_version").Groups[1].Value
     Assert('2.4.23' -eq $version) "Expecting apache v2.4.23, but instead got version $version"
 
     # apache2.4.25
-    (Get-Content config/software/apache.rb) `
-	  -replace 'default_version "2\.\d+\.\d+"', 'default_version "2.4.25"' `
+    (Get-Content config/software/apache.rb)
+    -replace 'default_version "2\.\d+\.\d+"', 'default_version "2.4.25"' `
 	  | Set-Content -Encoding ascii config/software/apache.rb
 
     ruby bin/omnibus build apache_upgrade_test --log-level warn
-    $msi=(gci $pwd/pkg/apache-upgrade-test*.msi | select fullname -last 1).FullName
+    $msi = (gci $pwd/pkg/apache-upgrade-test*.msi | select fullname -last 1).FullName
     rm -ea 0 -force install_apache25.log
     Start-Process -Wait -FilePath msiexec -ArgumentList /i, `
 	  "$msi", /qn, /l*v, install_apache25.log, PROJECTLOCATION='C:\windows\temp\apache-upgrade-test'
 
     $output = & "C:\windows\temp\apache-upgrade-test\apache\bin\httpd.exe" -version
     $regex_version = '((\d+\.)?(\d+\.)?(\*|\d+))'
-    $version = [regex]::match($output,"Apache/$regex_version").Groups[1].Value
+    $version = [regex]::match($output, "Apache/$regex_version").Groups[1].Value
     Assert('2.4.25' -eq $version) "Expecting apache v2.4.25, but instead got version $version"
 }
 
